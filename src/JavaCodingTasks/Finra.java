e5537e519c8c6b8a1c7fa7fad9a5f79159f8e3cf
KatiU <kati.isakova@gmail.com>
2023-09-15 17:42:00 -0700

    create class OddOrEven

--- src/JavaCodingTasks/Finra.java
+++ src/JavaCodingTasks/Finra.java
@@ -1,4 +1,27 @@
 package JavaCodingTasks;
 
 public class Finra {
+//FINRA :
+//Write a method which prints out the numbers from 1 to 30
+// but for numbers which are a multiple of 3print "FIN" instead of the number
+// and for numbers which are a multiple of 5, print "RA" instead of the number.
+// for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 30; i++) { // i: 1,2,3,... 100
+
+            if (i % 15 == 0) { // 15, 30
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) { //3, 6, 9, 12...
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) { //5, 10, 15, 20...
+                System.out.print("RA ");
+            } else { //1, 2, 4, 7 ....
+                System.out.print(i + " ");
+            }
+
+        }
+    }
 }
+
