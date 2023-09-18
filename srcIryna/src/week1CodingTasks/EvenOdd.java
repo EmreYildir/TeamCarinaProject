1df9de096875e8f882ac6aa6ab3fed2e87b2b298
Iryna Kondush <irynakondush@gmail.com>
2023-09-17 20:57:04 -0400

    added even/odd calculator

--- srcIryna/src/week1CodingTasks/EvenOdd.java
+++ srcIryna/src/week1CodingTasks/EvenOdd.java
@@ -1,5 +1,17 @@
 package week1CodingTasks;
 
 public class EvenOdd {
+    public static void main(String[] args) {
 
+        if(5 % 2 == 0){
+            System.out.println(5 + "=Even");
+        }else if(5 % 2 != 0){
+            System.out.println(5 + "=Odd");
+        }
+        if(6 % 2 == 0){
+            System.out.println(6 + "=Even");
+        }else if(6 % 2 != 0){
+            System.out.println(6 + "=Odd");
+        }
+    }
 }
