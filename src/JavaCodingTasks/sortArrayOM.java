a4b85bc2b20ad398d242c9a9075a7628b22ef273
Olegmengazov <olegmengazov@gmail.com>
2023-11-22 11:07:37 -0500

    added week 8 with arrays

--- src/JavaCodingTasks/sortArrayOM.java
+++ src/JavaCodingTasks/sortArrayOM.java
@@ -5,56 +5,43 @@ import java.util.Arrays;
 
 public class sortArrayOM {
     public static void main(String[] args) {
-        int[] givenArray = {123,-6,7,-8};
-        System.out.println(Arrays.toString(sortArray(givenArray)));
 
         int[] array = {64, 34, -25, 12, 22, 11, 90};
 
         System.out.println("Original array:");
         System.out.println(Arrays.toString(array));
 
-        bubbleSort(array);
-
         System.out.println("\nSorted array:");
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(sortArray(array)));
     }
-      public static int[] sortArray(int[] array) {
+
+    public static int[] sortArray(int[] array) {
         int[] sortedArray = new int[array.length];
         int minNumber = array[0];
-        for(int i =0; i<array.length;i++){
-            if(minNumber>array[i]){
+        for (int i = 0; i < array.length; i++) { //iteration thru the array to find the smallest number
+            if (minNumber > array[i]) {        // if my min number > current element than minNumber=current element
                 minNumber = array[i]; //-8
             }
         }
 
         for (int i = 0; i < array.length; i++) {
-            int maxNumber = minNumber;
+            int maxNumber = minNumber;    // first loop is used to reset my max number and index of the element
 
             int index = 0;
             for (int j = 0; j < array.length; j++) {   //{12,5,6,32}
-                    if (array[j] > maxNumber) {
-                        maxNumber = array[j];
-                        index = j;
-                    }
-                    if(j+1== array.length){
-                        sortedArray[i]=maxNumber;
-                        array[index]=minNumber;
-                    }
+                                                       //{0,5,6,0}
+                                                       //{32,12,0,0}
+                if (array[j] > maxNumber) {
+                    maxNumber = array[j];     //if element is bigger than max number we reasign biggest number to maxNumber and remember this index
+                    index = j;
                 }
-            }
-        return sortedArray;
-    }
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // swap arr[j+1] and arr[i]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if (j + 1 == array.length) {
+                    sortedArray[i] = maxNumber;    // if it's the end of the loop we add the biggest number into new sorted array and remove this number from existing one so we whould not check it again
+                    array[index] = minNumber;
                 }
             }
         }
+        return sortedArray;
     }
+
 }
